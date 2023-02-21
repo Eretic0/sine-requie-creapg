164fded8f15dc3709688a44419046c04ad8c125d
Eretic0 <87609853+Eretic0@users.noreply.github.com>
2023-02-21 16:49:04 +0100

    Professione slice upgrade

--- src/redux/slices/professioneSlice.js
+++ src/redux/slices/professioneSlice.js
@@ -2,6 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   professione: "",
+  professioneAbilitaScelte: [],
+  professioneAbilitaScelteLibere: [],
 };
 
 export const professioneSlice = createSlice({
@@ -14,9 +16,43 @@ export const professioneSlice = createSlice({
     resetProfessione: (state) => {
       state.professione = "";
     },
+    setProfessioneAbilitaScelte: (state, { payload }) => {
+      state.professioneAbilitaScelte = payload;
+    },
+    addProfessioneAbilitaScelta: (state, { payload }) => {
+      state.professioneAbilitaScelte.push(payload);
+    },
+    updateProfessioneAbilitaScelta: (state, { payload }) => {
+      state.professioneAbilitaScelte = state.professioneAbilitaScelte.map(
+        (ab) => (ab.idList === payload.idList ? payload : ab)
+      );
+    },
+
+    setProfessioneAbilitaScelteLibere: (state, { payload }) => {
+      state.professioneAbilitaScelteLibere = payload;
+    },
+
+    addProfessioneAbilitaSceltaLibera: (state, { payload }) => {
+      state.professioneAbilitaScelteLibere.push(payload);
+    },
+    updateProfessioneAbilitaSceltaLibera: (state, { payload }) => {
+      state.professioneAbilitaScelteLibere =
+        state.professioneAbilitaScelteLibere.map((ab) =>
+          ab.id === payload.id ? payload : ab
+        );
+    },
   },
 });
 
-export const { setProfessione, resetProfessione } = professioneSlice.actions;
+export const {
+  setProfessione,
+  resetProfessione,
+  setProfessioneAbilitaScelte,
+  addProfessioneAbilitaScelta,
+  updateProfessioneAbilitaScelta,
+  setProfessioneAbilitaScelteLibere,
+  addProfessioneAbilitaSceltaLibera,
+  updateProfessioneAbilitaSceltaLibera,
+} = professioneSlice.actions;
 
 export default professioneSlice.reducer;
