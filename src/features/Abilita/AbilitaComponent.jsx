95907ce0bc3dded6122bd08781fc1d24b3f4f657
Eretic0 <87609853+Eretic0@users.noreply.github.com>
2023-02-23 12:29:51 +0100

    Update AbilitaComponent.jsx

--- src/features/Abilita/AbilitaComponent.jsx
+++ src/features/Abilita/AbilitaComponent.jsx
@@ -1,64 +1,60 @@
 import FormControl from "@mui/material/FormControl";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormLabel from "@mui/material/FormLabel";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import Stack from "@mui/material/Stack";
-import React, { useEffect } from "react";
+import InputLabel from "@mui/material/InputLabel";
+import ListItemText from "@mui/material/ListItemText";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import AbilitaTable from "./AbilitaTable";
 import Card from "../../components/Card";
 import ProfiloAbilitaDb from "../../db/ProfiloAbilita";
 import {
+  resetAllAbilita,
+  setAbilita,
   setListBonusAbilita,
   setProfiloAbilitaSelezionato,
 } from "../../redux/slices/abilitaSlice";
+import AbilitaTable from "./AbilitaTable";
 
 function AbilitaComponent() {
-  const { abilita, profiloAbilitaSelezionato, listBonusAbilita } = useSelector(
-    (state) => state.abilita
-  );
+  const {
+    abilita,
+    profiloAbilitaSelezionato,
+    listBonusAbilita,
+    abilitaStoricoProfessione,
+  } = useSelector((state) => state.abilita);
   const dispatch = useDispatch();
-
-  useEffect(() => {}, []);
 
   const handleChangeProfiloAbilita = (event) => {
     const profiloAbilita = ProfiloAbilitaDb.find(
       (pa) => pa.id === event.target.value
     );
+    dispatch(resetAllAbilita());
+    dispatch(setAbilita(abilitaStoricoProfessione));
     dispatch(setProfiloAbilitaSelezionato(profiloAbilita.id));
     dispatch(setListBonusAbilita(profiloAbilita.arrayBonus));
   };
 
   return (
     <Card headerText="Abilità">
-      <Stack direction="row" spacing={2}>
-        <FormControl fullWidth>
-          <FormLabel id="abilitaprofiloscelta-radio-buttons-group-label">
-            Seleziona il profilo Abilità
-          </FormLabel>
-          <RadioGroup
-            name="abilitaprofiloscelta-radio-buttons-group"
-            value={profiloAbilitaSelezionato}
-            onChange={handleChangeProfiloAbilita}
-          >
-            {ProfiloAbilitaDb.map((pa) => (
-              <FormControlLabel
-                key={pa.id}
-                value={pa.id}
-                control={<Radio />}
-                label={
-                  <pre>
-                    {pa.nome}
-                    <br />
-                    {pa.descrizione}
-                  </pre>
-                }
-              />
-            ))}
-          </RadioGroup>
-        </FormControl>
-      </Stack>
+      <FormControl fullWidth sx={{ marginBottom: "8px" }}>
+        <InputLabel id="label-input-select-abilitaprofiloscelta">
+          Seleziona il profilo Abilità
+        </InputLabel>
+        <Select
+          id="select-abilitaprofiloscelta"
+          labelId="label-input-select-abilitaprofiloscelta"
+          defaultValue=""
+          value={profiloAbilitaSelezionato}
+          label="Seleziona il profilo Abilità"
+          onChange={handleChangeProfiloAbilita}
+        >
+          {ProfiloAbilitaDb.map((pa) => (
+            <MenuItem value={pa.id} key={pa.id}>
+              <ListItemText primary={pa.nome} secondary={pa.descrizione} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
       <AbilitaTable
         abilita={abilita}
         listBonusAbilita={listBonusAbilita}
